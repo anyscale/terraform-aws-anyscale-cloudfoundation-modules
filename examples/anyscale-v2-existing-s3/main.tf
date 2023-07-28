eea8bf6069fd97ecb1a4d74ecf011a87ea071059
Brent Bain <brent@anyscale.com>
2023-07-28 14:25:58 -0600

    [add] example - anyscale-v2-existing-s3
    Add an example of using an existing S3 bucket with these Terraform modules.
    This skips creating the S3 bucket and instead uses an existing bucket.
    
    On branch brent/add-existings3-example
    Changes to be committed:
    	new file:   examples/anyscale-v2-existing-s3/README.md
    	new file:   examples/anyscale-v2-existing-s3/main.tf
    	new file:   examples/anyscale-v2-existing-s3/outputs.tf
    	new file:   examples/anyscale-v2-existing-s3/variables.tf
    	new file:   examples/anyscale-v2-existing-s3/versions.tf

---
+++ examples/anyscale-v2-existing-s3/main.tf
@@ -0,0 +1,44 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# Create core Anyscale v2 Stack resources with minimal parameters but a common name
+#   Should be executed in us-east-2
+#   Creates a v2 stack including
+#     - IAM Roles
+#     - S3 Bucket
+#     - VPC with publicly routed subnets (no private subnets)
+#     - VPC Security Groups
+#     - EFS
+# ---------------------------------------------------------------------------------------------------------------------
+locals {
+  full_tags = merge(tomap({
+    anyscale-cloud-id           = var.anyscale_cloud_id,
+    anyscale-deploy-environment = var.anyscale_deploy_env
+    }),
+    var.tags
+  )
+}
+
+module "aws_anyscale_v2_common_name" {
+  source = "../.." #this should be changed if executing this example outside of this repository
+  tags   = local.full_tags
+
+  anyscale_deploy_env = var.anyscale_deploy_env
+  anyscale_cloud_id   = var.anyscale_cloud_id
+  anyscale_org_id     = var.anyscale_org_id
+
+  create_cluster_node_cloudwatch_policy = true
+
+  existing_s3_bucket_arn = var.existing_s3_bucket_arn
+
+  # VPC Related
+  anyscale_vpc_cidr_block     = "172.24.0.0/16"
+  anyscale_vpc_public_subnets = ["172.24.21.0/24", "172.24.22.0/24", "172.24.23.0/24"]
+  # anyscale_vpc_private_subnets = ["172.24.101.0/24", "172.24.102.0/24", "172.24.103.0/24"]
+
+  common_prefix   = var.common_prefix
+  use_common_name = true
+
+  # Security Group Related
+  security_group_ingress_allow_access_from_cidr_range = var.customer_ingress_cidr_ranges
+
+
+}
