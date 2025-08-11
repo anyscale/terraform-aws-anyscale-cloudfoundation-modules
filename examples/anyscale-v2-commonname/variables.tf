5106b13e068daebc75d1deef6aa11ff92707acf8
Brent Bain <brent@anyscale.com>
2025-08-11 08:42:21 -0800

    upd: CommonName example to move org id to required variable
    Changes to be committed:
    	modified:   examples/anyscale-v2-commonname/README.md
    	modified:   examples/anyscale-v2-commonname/variables.tf

--- examples/anyscale-v2-commonname/variables.tf
+++ examples/anyscale-v2-commonname/variables.tf
@@ -52,6 +52,30 @@ variable "anyscale_external_id" {
   type        = string
 }
 
+variable "anyscale_org_id" {
+  description = <<-EOT
+    (Optional) Anyscale Organization ID.
+
+    This is used to lock down the cross account access role by Organization ID. Because the Organization ID is unique to each
+    customer, this ensures that only the customer can access their own resources.
+
+    ex:
+    ```
+    anyscale_org_id = "org_abcdefghijklmn1234567890"
+    ```
+  EOT
+  type        = string
+  validation {
+    condition = (
+      var.anyscale_org_id == null ? true : (
+        length(var.anyscale_org_id) > 4 &&
+        substr(var.anyscale_org_id, 0, 4) == "org_"
+      )
+    )
+    error_message = "The anyscale_org_id value must start with \"org_\"."
+  }
+}
+
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These variables have defaults, but may be overridden.
@@ -98,30 +122,7 @@ variable "anyscale_cloud_id" {
   }
 }
 
-variable "anyscale_org_id" {
-  description = <<-EOT
-    (Optional) Anyscale Organization ID.
 
-    This is used to lock down the cross account access role by Organization ID. Because the Organization ID is unique to each
-    customer, this ensures that only the customer can access their own resources.
-
-    ex:
-    ```
-    anyscale_org_id = "org_abcdefghijklmn1234567890"
-    ```
-  EOT
-  type        = string
-  default     = null
-  validation {
-    condition = (
-      var.anyscale_org_id == null ? true : (
-        length(var.anyscale_org_id) > 4 &&
-        substr(var.anyscale_org_id, 0, 4) == "org_"
-      )
-    )
-    error_message = "The anyscale_org_id value must start with \"org_\"."
-  }
-}
 
 variable "tags" {
   description = <<-EOF
